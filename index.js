9310e9824dad00bebab4d9fbdfa6925bb9533e98
Zed128 <118735074+Zed128@users.noreply.github.com>
2023-06-30 12:26:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const fetch = require("node-fetch");
 
+async function main() {
+  const response = await fetch('https://reqtest.zed256.repl.co');
+  const body = await response.text();
+
+  console.log(body)
+  setTimeout(main, 1600);
+}
+
+main()
